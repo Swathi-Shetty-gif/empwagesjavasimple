e87a644be6c3c008e31861a0de4df1edddc02207
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-21 12:04:42 +0530

    added: working days in moth

--- src/com/bridgelabz/EmpSimple.java
+++ src/com/bridgelabz/EmpSimple.java
@@ -3,16 +3,18 @@ package com.bridgelabz;
 public class EmpSimple {
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
-
+	public static final int EMP_RATE_PER_HOURS=20;
+	public static final int NUMBER_WORKING_DAYS=20;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int random =(int) Math.floor(Math.random() * 10 ) % 3;
+		
 			
-			
-			int emphrs=0;
-			int EMP_RATE_PER_HOURS=20;
-			int empwages=0;
+			int workingdays=20;
+			int emphrs=0,empwages=0,totalEmpwage=0;
+			for (int i=0;i<=NUMBER_WORKING_DAYS;i++)
+			{
+				int random =(int) Math.floor(Math.random() * 10 ) % 3;
 			switch (random)
 			{
 			
@@ -26,8 +28,11 @@ public class EmpSimple {
 				
 			}
 			empwages=emphrs*EMP_RATE_PER_HOURS;
-			System.out.println("emp wages" + empwages);
+			totalEmpwage +=empwages;
+			System.out.println("total empwages" + empwages);
 			
+	}
+			System.out.println("total working days" + totalEmpwage);
 	}
 
 }
