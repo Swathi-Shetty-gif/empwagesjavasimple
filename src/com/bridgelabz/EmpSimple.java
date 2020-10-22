df2b206fad509684fa10f39acda2b5477e3de099
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-22 19:09:37 +0530

    added:class and methods to totwl working days

--- src/com/bridgelabz/EmpSimple.java
+++ src/com/bridgelabz/EmpSimple.java
@@ -5,33 +5,33 @@ public class EmpSimple{
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HOURS=20;
 	public static final int NUMBER_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=10;
 	static void  EmpPresent() {
 
-		int workingdays=20;
-		int emphrs=0,empwages=0,totalEmpwage=0;
-		for (int i=0;i<=NUMBER_WORKING_DAYS;i++)
-		{
-			int random =(int) Math.floor(Math.random() * 10 ) % 3;
-		switch (random)
-		{
-		
-			case 1:	emphrs=80;
-					break;
-		
-			case 2: emphrs=40;
-					break;
-			
-			default:emphrs=0;//not present
-			
-		}
-		empwages=emphrs*EMP_RATE_PER_HOURS;
-		totalEmpwage +=empwages;
-		System.out.println("total empwages" + empwages);
-		
-}
-		System.out.println("total working days" + totalEmpwage);
-}
-
+		// TODO Auto-generated method stub
+				int emphrs=0,totalworkingdays=0,totalEmphrs=0;
+					while(totalEmphrs<MAX_HRS_IN_MONTH && totalworkingdays<NUMBER_WORKING_DAYS)
+					{
+						totalEmphrs++;
+						int random =(int) Math.floor(Math.random() * 10 ) % 3;
+					switch (random)
+					{
+					
+						case 1:	emphrs=80;
+								break;
+					
+						case 2: emphrs=40;
+								break;
+						
+						default:emphrs=0;//not present
+						
+					}
+					totalEmphrs +=emphrs;
+					System.out.println("total empwages" + emphrs);	
+			}
+						int totalEmpWage = totalEmphrs*EMP_RATE_PER_HOURS;
+					System.out.println("total working days" + totalEmpWage);
+			}
 			
 	
 public static void main(String[] args) {
