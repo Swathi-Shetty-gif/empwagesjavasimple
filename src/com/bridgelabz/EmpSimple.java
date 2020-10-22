a68d9a52c03977bbc4427d168ce4fa5ae4c88dba
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-22 18:53:06 +0530

    added:class and method to switch cases

--- src/com/bridgelabz/EmpSimple.java
+++ src/com/bridgelabz/EmpSimple.java
@@ -1,32 +1,28 @@
 package com.bridgelabz;
 
 public class EmpSimple{
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
 	static void  EmpPresent() {
-		double random = Math.floor(Math.random() * 10 ) % 3;
-		
-		int IS_FULL_TIME=1;
-		int IS_PART_TIME=2;
-		int emphrs=0;
-		int EMP_RATE_PER_HOURS=20;
-		int empwages=0;
-		if(random==IS_FULL_TIME)
-		{
-			emphrs=80;
+
+		int random =(int) Math.floor(Math.random() * 10 ) % 3;
+			int emphrs=0;
+			int EMP_RATE_PER_HOURS=20;
+			int empwages=0;
+			switch (random) {
+			case 1:	emphrs=80;
+						break;
 			
-		}
-		else if(random==IS_PART_TIME)
-		{
-			emphrs=40;
-		}
-		else
-		{
-			emphrs=0;//not present
+				case 2: emphrs=40;
+						break;
+				
+				default:emphrs=0;//not present
+				
+			}
+			empwages=emphrs*EMP_RATE_PER_HOURS;
+			System.out.println("emp wages" + empwages);
 			
-		}
-		empwages=emphrs*EMP_RATE_PER_HOURS;
-		System.out.println("emp wages" + empwages);
-		
-}
+	}
 public static void main(String[] args) {
 	EmpSimple obj1=new EmpSimple();
 		obj1.EmpPresent();
