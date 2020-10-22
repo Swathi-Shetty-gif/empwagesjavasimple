38520232c5e75c39e134aecd74fc7044c72dc5c2
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-22 18:41:51 +0530

    added:class and method to parttime

--- src/com/bridgelabz/EmpSimple.java
+++ src/com/bridgelabz/EmpSimple.java
@@ -2,24 +2,30 @@ package com.bridgelabz;
 
 public class EmpSimple{
 	static void  EmpPresent() {
-	double random = Math.floor(Math.random() * 10 ) % 2;
-	int IS_PRESENT=1;
-	int emphrs=0;
-	int EMP_RATE_PER_HOURS=20;
-	int empwages=0;
-	if(random==IS_PRESENT)
-	{
-		emphrs=80;
+		double random = Math.floor(Math.random() * 10 ) % 3;
 		
-	}
-	else
-	{
-		emphrs=0;//not present
+		int IS_FULL_TIME=1;
+		int IS_PART_TIME=2;
+		int emphrs=0;
+		int EMP_RATE_PER_HOURS=20;
+		int empwages=0;
+		if(random==IS_FULL_TIME)
+		{
+			emphrs=80;
+			
+		}
+		else if(random==IS_PART_TIME)
+		{
+			emphrs=40;
+		}
+		else
+		{
+			emphrs=0;//not present
+			
+		}
+		empwages=emphrs*EMP_RATE_PER_HOURS;
+		System.out.println("emp wages" + empwages);
 		
-	}
-	empwages=emphrs*EMP_RATE_PER_HOURS;
-	System.out.println("emp wages" + empwages);
-	
 }
 public static void main(String[] args) {
 	EmpSimple obj1=new EmpSimple();
