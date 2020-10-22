ba38a44f9b8c800509363d742e6d5f4642245c3f
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-22 18:32:05 +0530

    added:method and class employee wages

--- src/com/bridgelabz/EmpSimple.java
+++ src/com/bridgelabz/EmpSimple.java
@@ -4,15 +4,23 @@ public class EmpSimple{
 	static void  EmpPresent() {
 	double random = Math.floor(Math.random() * 10 ) % 2;
 	int IS_PRESENT=1;
+	int emphrs=0;
+	int EMP_RATE_PER_HOURS=20;
+	int empwages=0;
 	if(random==IS_PRESENT)
 	{
-		System.out.println("present");
+		emphrs=80;
+		
 	}
 	else
 	{
-		System.out.println("not present");
+		emphrs=0;//not present
+		
 	}
-	}
+	empwages=emphrs*EMP_RATE_PER_HOURS;
+	System.out.println("emp wages" + empwages);
+	
+}
 public static void main(String[] args) {
 	EmpSimple obj1=new EmpSimple();
 		obj1.EmpPresent();
