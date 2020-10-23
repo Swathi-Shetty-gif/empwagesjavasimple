7378c70a5482f7be718c9f1f550bf2784980d79f
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-23 21:25:50 +0530

    added:multiple comapies added to the computation

--- src/com/bridgelabz/EmpSimple.java
+++ src/com/bridgelabz/EmpSimple.java
@@ -1,67 +1,48 @@
 package com.bridgelabz;
 
-public class EmpSimple{
-	public static final int IS_FULL_TIME=1;
-	public static final int IS_PART_TIME=2;
+public class EmpSimple {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 	private final int EMP_RATE_PER_HOURS = 0;
 	private final int NUMBER_WORKING_DAYS = 0;
 	private final int MAX_HRS_IN_MONTH = 0;
 	private final String company = "";
-	public static int ComputeWage(String company,int EMP_RATE_PER_HOURS,int NUMBER_WORKING_DAYS,int MAX_HRS_IN_MONTH)
-	{
-		int emphrs=0,totalworkingdays=0,totalEmphrs=0;
-		while(totalEmphrs<MAX_HRS_IN_MONTH && totalworkingdays<NUMBER_WORKING_DAYS)
-		{
-			totalEmphrs++;
-			int random =(int) Math.floor(Math.random() * 10 ) % 3;
-		switch (random)
-		{
-		
-			case 1:	emphrs=80;
-					break;
-		
-			case 2: emphrs=40;
-					break;
-			
-			default:emphrs=0;//not present
-			
-		}
-		totalEmphrs +=emphrs;
-		System.out.println("total empwages" + emphrs);	
-}
-			int totalEmpWage = totalEmphrs*EMP_RATE_PER_HOURS;
-		System.out.println("total working days" + totalEmpWage);
-		return  totalEmpWage;
-}
 
-	
-public static void main(String[] args) {
-	ComputeWage("infosys", 20,2,10);
-	ComputeWage("reliance",10,4,20);
-	ComputeWage("tata",10,4,20);
-	ComputeWage("bridgelabz",10,4,20);
+	public static int ComputeWage(String company, int EMP_RATE_PER_HOURS, int NUMBER_WORKING_DAYS,
+			int MAX_HRS_IN_MONTH) {
+		int emphrs = 0, totalworkingdays = 0, totalEmphrs = 0;
+		while (totalEmphrs < MAX_HRS_IN_MONTH && totalworkingdays < NUMBER_WORKING_DAYS) {
+			totalEmphrs++;
+			int random = (int) Math.floor(Math.random() * 10) % 3;
+			switch (random) {
+
+			case 1:
+				emphrs = 80;
+				break;
+
+			case 2:
+				emphrs = 40;
+				break;
+
+			default:
+				emphrs = 0;// not present
+
+			}
+			totalEmphrs += emphrs;
+			System.out.println("total empwages" + emphrs);
+		}
+		int totalEmpWage = totalEmphrs * EMP_RATE_PER_HOURS;
+		System.out.println("total working days" + totalEmpWage);
+		return totalEmpWage;
+	}
+
+	public static void main(String[] args) {
+		ComputeWage("infosys", 20, 2, 10);
+		ComputeWage("reliance", 10, 4, 20);
+		ComputeWage("tata", 10, 4, 20);
+		ComputeWage("bridgelabz", 10, 4, 20);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //package com.bridgelabz;
 //
