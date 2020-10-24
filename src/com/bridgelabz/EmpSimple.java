a660339c724362991633a8261c242cb1086cab40
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-24 18:42:56 +0530

    added: total employee wages

--- src/com/bridgelabz/EmpSimple.java
+++ src/com/bridgelabz/EmpSimple.java
@@ -1,46 +1,58 @@
 package com.bridgelabz;
-import java.util.HashMap;
 
 public class EmpSimple {
-	public static final int FULL_TIME = 1;
-	public static final int PART_TIME=2;
-	public static void main(String[] args) {
-		EmpSimple ew=new EmpSimple();
-		ew.WagesCalculation("infosys", 20);
-		ew.WagesCalculation("bridgelabz", 40);
-		ew.WagesCalculation("wipro", 60);
-		ew.WagesCalculation("cognizath", 80);
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxhoursPerMonth;
+	private final String company;
+	private int totalEmpWage;
+	public EmpSimple(String company, int empRatePerHour, int numOfWorkingDays, int maxhoursPerMonth)
+	{
+		this.company=company;
+		this.empRatePerHour=empRatePerHour;
+		this.numOfWorkingDays=numOfWorkingDays;
+		this.maxhoursPerMonth=maxhoursPerMonth;
 	}
-		public void WagesCalculation(String company,int Wages_Per_Hour ){
-			
-			
-			int full_Day_Hour=8;
-			int Part_Day_Hour=4;
-			int days=20;
-			int workingDays=1;
-			int workingHour=0;
-			int totalWages=0;
-			while (workingDays <= days && workingHour <100 ) {
-			int checkout=(int) (Math.floor(Math.random() *10) %2+1);
-			switch(checkout){
-				case  FULL_TIME:int total=  Wages_Per_Hour * full_Day_Hour;
-									workingHour=workingHour+full_Day_Hour;
-									totalWages=totalWages+total;
-									break;
-				case PART_TIME :int total1= Wages_Per_Hour * Part_Day_Hour;
-									workingHour=workingHour + Part_Day_Hour;
-									totalWages=totalWages+total1;
-									break;
-				default : System.out.println("0");
+	public void computeEmpWage() {
+		int emphrs = 0, totalworkingdays = 0, totalEmphrs = 0;
+		while (totalEmphrs < maxhoursPerMonth && totalworkingdays < numOfWorkingDays) {
+			totalworkingdays++;
+			int random = (int) Math.floor(Math.random() * 10) % 3;
+			switch (random) {
+
+			case 1:
+				emphrs = 8;
+				break;
+
+			case 2:
+				emphrs = 4;
+				break;
+
+			default:
+				emphrs = 0;// not present
+
 			}
-			workingDays++;					
-			}
-			
-			HashMap<String, Integer> map = new HashMap<String, Integer>();
-			map.put(company, totalWages);
-			for (String i : map.keySet()) {
-			      System.out.println( i + " wages in month " + map.get(i));
+			totalEmphrs += emphrs;
+			System.out.println("days" +totalworkingdays+ "Emphrs" +emphrs);
 		}
-		}
-	
+		totalEmpWage = totalEmphrs * empRatePerHour;
+		
+	}
+	//@override
+	public String toString(){
+	return "Total Emp wages for company " +company+ "is" +totalEmpWage;
+	}
+
+	public static void main(String[] args) {
+		EmpSimple info = new EmpSimple("infosys" ,20,2,10);
+		EmpSimple cogni = new EmpSimple("cognizath" ,40,4,20);
+		info.computeEmpWage();
+		System.out.println(info);
+		cogni.computeEmpWage();
+		System.out.println(cogni);
+		
+	}
 }
