9671e03a916a1777c7520118019de5c3179f26aa
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-24 07:36:41 +0530

    added:the hashmap

--- src/com/bridgelabz/EmpSimple.java
+++ src/com/bridgelabz/EmpSimple.java
@@ -1,41 +1,46 @@
 package com.bridgelabz;
+import java.util.HashMap;
 
-public class EmpSimple{
-	public static final int IS_FULL_TIME=1;
-	public static final int IS_PART_TIME=2;
-	public static final int EMP_RATE_PER_HOURS=20;
-	public static final int NUMBER_WORKING_DAYS=20;
-	public static final int MAX_HRS_IN_MONTH=10;
-	static void  EmpPresent() {
-
-		int emphrs=0,totalworkingdays=0,totalEmphrs=0;
-		while(totalEmphrs<MAX_HRS_IN_MONTH && totalworkingdays<NUMBER_WORKING_DAYS)
-		{
-			totalEmphrs++;
-			int random =(int) Math.floor(Math.random() * 10 ) % 3;
-		switch (random)
-		{
-		
-			case 1:	emphrs=80;
-					break;
-		
-			case 2: emphrs=40;
-					break;
-			
-			default:emphrs=0;//not present
-			
-		}
-		totalEmphrs +=emphrs;
-		System.out.println("total empwages" + emphrs);	
-}
-			int totalEmpWage = totalEmphrs*EMP_RATE_PER_HOURS;
-		System.out.println("total working days" + totalEmpWage);
-}
-	
-public static void main(String[] args) {
-	EmpSimple obj1=new EmpSimple();
-		obj1.EmpPresent();
+public class EmpSimple {
+	public static final int FULL_TIME = 1;
+	public static final int PART_TIME=2;
+	public static void main(String[] args) {
+		EmpSimple ew=new EmpSimple();
+		ew.WagesCalculation("infosys", 20);
+		ew.WagesCalculation("bridgelabz", 40);
+		ew.WagesCalculation("wipro", 60);
+		ew.WagesCalculation("cognizath", 80);
 	}
+		public void WagesCalculation(String company,int Wages_Per_Hour ){
+			
+			
+			int full_Day_Hour=8;
+			int Part_Day_Hour=4;
+			int days=20;
+			int workingDays=1;
+			int workingHour=0;
+			int totalWages=0;
+			while (workingDays <= days && workingHour <100 ) {
+			int checkout=(int) (Math.floor(Math.random() *10) %2+1);
+			switch(checkout){
+				case  FULL_TIME:int total=  Wages_Per_Hour * full_Day_Hour;
+									workingHour=workingHour+full_Day_Hour;
+									totalWages=totalWages+total;
+									break;
+				case PART_TIME :int total1= Wages_Per_Hour * Part_Day_Hour;
+									workingHour=workingHour + Part_Day_Hour;
+									totalWages=totalWages+total1;
+									break;
+				default : System.out.println("0");
+			}
+			workingDays++;					
+			}
+			
+			HashMap<String, Integer> cm = new HashMap<String, Integer>();
+			cm.put(company, totalWages);
+			for (String i : cm.keySet()) {
+			      System.out.println( i + " wages in month " + cm.get(i));
+		}
+		}
+	
 }
-
-
