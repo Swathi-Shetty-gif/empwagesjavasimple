4bdd893b3ca0d51e316da7fb3938a60d8ced17f1
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-26 07:57:23 +0530

    added:total wages with querried company

---
+++ src/uc14/IComputeEmpWage.java
@@ -0,0 +1,8 @@
+package uc14;
+
+
+public interface IComputeEmpWage {
+	public void addCompanyEmpWage(String company,int empRateperHour,int workingDays,int maxHours);
+	public void computeEmpWage();
+	public int getTotalWage(String company);
+	}
